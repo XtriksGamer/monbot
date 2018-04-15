fbe1a71eb091df5f40bada39e8753d30e9c752df
XtriksGamer <38396744+XtriksGamer@users.noreply.github.com>
2018-04-15 12:20:10 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 
 var bot = new Discord.Client();
-var prefix = ("ed!");
+var prefix = ("m!");
   
 
 bot.login('NDM0ODQ4MzI0MjA2NTkyMDE5.DbSwHw.NHtgcOV7IYHZ5ZNUmlDtgPDR4Sg')
 console.log("Caca")
 
 bot.on('ready', function (){
-    bot.user.setPresence({ game: { name: "TU PEUT MODIFER"}})
+    bot.user.setPresence({ game: { name: "| Bot Moderator |"}})
 })
