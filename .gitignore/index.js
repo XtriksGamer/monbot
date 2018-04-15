de8ab9a94b3960b69dab865471ff12c514fd5242
XtriksGamer <38396744+XtriksGamer@users.noreply.github.com>
2018-04-15 12:28:39 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ var bot = new Discord.Client();
 var prefix = ("m!");
   
 
-bot.login('NDM0ODQ4MzI0MjA2NTkyMDE5.DbSwHw.NHtgcOV7IYHZ5ZNUmlDtgPDR4Sg')
+bot.login(process.env.TOKEN)
 console.log("Caca")
 
 bot.on('ready', function (){
