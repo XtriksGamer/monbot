3d28f27f72f594315fc6f8d31e70a88faa08c4a3
XtriksGamer <38396744+XtriksGamer@users.noreply.github.com>
2018-04-15 12:21:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -10,3 +10,11 @@ console.log("Caca")
 bot.on('ready', function (){
     bot.user.setPresence({ game: { name: "| Bot Moderator |"}})
 })
+
+  if (message.content === (prefix + "help")){
+        var help_embed = new Discord.RichEmbed()
+           .setColor('#269f26')
+            .addField("[**Développement**]", "Désolé cette commande es en developement")
+           .setFooter("moderator bot")        
+           message.channel.send(help_embed)
+    }
